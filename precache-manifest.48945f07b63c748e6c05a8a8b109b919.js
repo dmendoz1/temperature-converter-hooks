6be320d552431ac9409a855f953543a52a26f78d
Daniel Mendoza <dmendoza@fb.com>
2019-08-06 16:09:58 -0700

    Updates

---
+++ precache-manifest.48945f07b63c748e6c05a8a8b109b919.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19e534a78d91a92fe37404e1fc68280f",
+    "url": "/temperature-converter-hooks/index.html"
+  },
+  {
+    "revision": "728e93df4eec9344b0fa",
+    "url": "/temperature-converter-hooks/static/css/main.8aca12c9.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/temperature-converter-hooks/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "728e93df4eec9344b0fa",
+    "url": "/temperature-converter-hooks/static/js/main.54ed3e19.chunk.js"
+  },
+  {
+    "revision": "ca5970cdf684d2c23448",
+    "url": "/temperature-converter-hooks/static/js/runtime~main.08869b74.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/temperature-converter-hooks/precache-manifest.48945f07b63c748e6c05a8a8b109b919.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/temperature-converter-hooks/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
